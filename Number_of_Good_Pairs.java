012bc8759e935494c5771b3d646f01b918d8d9f9
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-20 22:26:40 +0530

    Number of Good Pairs

---
+++ Number_of_Good_Pairs.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class Number_of_Good_Pairs {
+
+    public int numIdenticalPairs(int[] nums) {
+        int n= nums.length;
+        int count =0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(nums[i] == nums[j] && i<j)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+}
