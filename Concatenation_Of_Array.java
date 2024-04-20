c13a45634de0d05abd13d8bb4c7ef9188ae7fd70
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-20 21:17:16 +0530

    Solution of Concatenation of Array

---
+++ Concatenation_Of_Array.java
@@ -0,0 +1,14 @@
+class Concatenation_of_Array
+{
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+       int[] ans = new int[n * 2];
+
+    for(int i=0; i<n; i++){
+        ans[i] = nums[i];
+         ans[i+n] = nums[i];
+    }
+    return ans;
+       
+    }
+}
