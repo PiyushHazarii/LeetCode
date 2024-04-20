f7a8d1601c4dc86b6d398b263a43852252f01d05
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-20 23:14:01 +0530

    Solution in java

---
+++ Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+public class Kids_With_the_Greatest_Number_of_Candies {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+
+        int n = candies.length;
+        int maxcandvalue = -1;
+        for(int i=0;i<n; i++)
+        {
+            if(candies[i] > maxcandvalue)
+            {
+                maxcandvalue = candies[i];
+            }
+        }
+
+
+        ArrayList<Boolean> result = new ArrayList<>();
+
+
+        for(int i = 0 ;i<n; i++)
+        {
+            if(candies[i]+extraCandies >= maxcandvalue)
+            {
+                result.add(true);
+            }
+            else
+            {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
