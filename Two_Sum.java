1dfaf513bd05586069dab20292e6f7e8ab2044e2
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-20 21:03:59 +0530

    Two Sum solution in java

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,5 +1,50 @@
 package LeetCode;
 
 public class Two_Sum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+
+         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> BRUTE FORCE METHOD <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+       int n = nums.length;
+       int result[] = new int[2];
+       for(int i=0;i<n-1;i++)
+       {
+           for(int j=i+1;j<n;j++)
+           {
+               if(nums[i] + nums[j] == target)
+               {
+                   result[0] = i;
+                   result[1] = j; ￼
+               }
+           }
+       return result; 
+
+
+
+
+
+       >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Efficient Way <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+
+       int n = nums.length;
+       HashMap<Integer , Integer> map = new HashMap<>();
+       for(int i=0;i<n;i++)
+       {
+           int remaining = target - nums[i];
+           if(map.containsKey(remaining))
+           {
+               return new int[]{map.get(remaining),i};
+           }
+           map.put(nums[i],i);
+       }
+       return new int[]{};
+   }
+}
     
 }
