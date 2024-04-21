da305efb5e0a020341d3d1f726c48637f39dbaf9
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-21 20:19:27 +0530

    Solution in java

---
+++ Truncate_Sentence.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class Truncate_Sentence {
+
+    public String truncateSentence(String s, int k) {
+
+        String[] words = s.split(" ");
+        StringBuilder truncated = new StringBuilder();  
+
+        for (int i = 0; i < k; i++) 
+        {
+           truncated.append(words[i]);
+           if(i < k-1)
+            {
+                truncated.append(" ");
+            }
+        }
+
+       return truncated.toString();
+   }
+    
+}
