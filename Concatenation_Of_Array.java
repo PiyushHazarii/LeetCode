d45344c4861ea636a4d979c1069db1bdf4dc5293
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-21 20:50:00 +0530

    Solution in java

--- Concatenation_Of_Array.java
+++ Concatenation_Of_Array.java
@@ -1,13 +1,16 @@
 class Concatenation_of_Array
 {
-    public int[] getConcatenation(int[] nums) {
+    public int[] getConcatenation(int[] nums) 
+    {
         int n = nums.length;
        int[] ans = new int[n * 2];
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
+    {
         ans[i] = nums[i];
          ans[i+n] = nums[i];
     }
+    
     return ans;
        
     }
