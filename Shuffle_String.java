390c0ce782c4812beb75e0da8f7ec1cc4869fb93
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-21 20:21:06 +0530

    Solution in java

---
+++ Shuffle_String.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class Shuffle_String {
+
+
+    public String restoreString(String s, int[] indices) {
+
+        char[] shuffled = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) 
+        {
+            shuffled[indices[i]] = s.charAt(i);
+        }
+
+        return new String(shuffled);
+        
+    }
+    
+}
