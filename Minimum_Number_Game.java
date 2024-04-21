555d156ae4dde5905688a6fea09b23c64f44f7ce
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-21 15:45:58 +0530

    Solution in java

---
+++ Minimum_Number_Game.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class Minimum_Number_Game {
+
+    public int[] numberGame(int[] nums) {
+
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for(int i=0;i<n;i+=2)
+        {
+            int temp;
+            if(nums[i] <  nums[i+1])
+            {
+                temp = nums[i];
+                nums[i]= nums[i+1];
+                nums[i+1] = temp;
+            }
+        }
+
+        return nums;
+        
+    }
+    
+}
