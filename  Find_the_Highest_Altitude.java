f6ced744066c7b814fb5d0e868af4d51d4ed80f8
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-23 00:56:09 +0530

    Solution in java

---
+++  Find_the_Highest_Altitude.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class  Find_the_Highest_Altitude {
+
+    public int largestAltitude(int[] gain) 
+    {
+
+        int maxAltitude = 0;
+        int currentAltitude = 0;
+        
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+        
+        return maxAltitude;
+        
+    }
+    
+}
