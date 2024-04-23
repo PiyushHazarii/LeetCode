af1b470cf32d4559bdf761c4a0a6cb145a13ce8c
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-24 01:33:56 +0530

    Solution in java

---
+++ Destination_City.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class Destination_City {
+
+    public String destCity(List<List<String>> paths) 
+    {
+       int n = paths.size();
+
+       Set<String> sets = new HashSet<>();
+
+       for(int i=0;i<n;i++)
+       {
+            sets.add(paths.get(i).get(0));
+       }
+
+       for(int j=0;j<n;j++)
+       {
+            String destination = paths.get(j).get(1);
+            if(!sets.contains(destination))
+            {
+                return destination;
+            }
+       }
+
+       return "";
+       
+    }
+    
+}
