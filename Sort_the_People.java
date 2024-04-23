993cb7eaae72adef9d129add5cc923686ad19e81
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-24 01:32:31 +0530

    Solution in java

---
+++ Sort_the_People.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+public class Sort_the_People {
+
+    public String[] sortPeople(String[] names, int[] heights) {
+
+        int n = names.length;
+
+        HashMap<Integer , String> hs = new HashMap<>();
+
+        for(int i=0;i<n;i++)
+        {
+            hs.put(heights[i] , names[i]);
+        }
+
+        Arrays.sort(heights);
+
+        String[] sorted= new String[n];
+
+        int index =0;
+
+        for(int i=heights.length-1;i>=0;i--)
+        {
+            sorted[index] = hs.get(heights[i]);
+            index++;
+        }
+
+        return sorted;
+        
+    }
+    
+}
