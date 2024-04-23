7053f743f7ec7681136a068f3fafb34514e64bb3
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-04-24 01:24:28 +0530

    Solution in java

---
+++ Sum_of_All_Subset_XOR_Totals.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class Sum_of_All_Subset_XOR_Totals {
+
+    public int subsetXORSum(int[] nums) 
+    {
+        return helper(nums, 0, 0 );
+    }
+
+    public int helper(int nums[], int level , int currentXOR)
+    {
+        if(level == nums.length)
+        {
+            return currentXOR;
+        }
+
+        int include = helper(nums , level+1 , currentXOR ^ nums[level]);
+        int exclude = helper(nums , level+1 , currentXOR);
+
+        return include + exclude;
+        
+    }
+    
+}
